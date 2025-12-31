f195d4db509eb3fbfc244fd86d602321dd416278
Chaimcv <chaithanyamcv@gmail.com>
2025-12-31 13:14:04 +0530

    additons

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const products=fetch('https://dummyjson.com/products')
               .then(res => res.json())
               .then(products=>{
                 console.log(products);
-console.log(products);
+console.log(products,"dis");
 const main=products.map(item=>{
-    return `
+    return`
     <div class="card"{key=item.id}>
-    
-    <h3 class="card_title">Title:${item?.title}</h3>
+    // <img class="img" src=${item.images}>
+    <h3 class="card-title">Title:${item?.title}</h3>
     </div>
     `;}).join("");
 document.getElementById("main").innerHTML="main";
