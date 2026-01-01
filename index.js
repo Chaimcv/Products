6554d4b3401c7cfa9a3add0507dacf8f674b274f
Chaimcv <chaithanyamcv@gmail.com>
2026-01-01 19:51:56 +0530

    html page updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const product=fetch('https://dummyjson.com/products')
 const display=product.products.map(item=>{
     return`
     <div class="card h-70 bg-slate-200 outline-10 outline-white text-xs md:text-2xl" ${key=item.id}>
-    <img class="img w-60 h-50 " src=${item?.images} alt=${item?.title}>
+    <img class="img w-60 h-40 " src=${item?.images} alt=${item?.title}>
     <h3 class="card-title text-center si">${item?.title}</h3>
     </div>
     `;}).join("");
